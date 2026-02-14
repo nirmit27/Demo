850c2e2670615501fdf6fb7e4389412484268f10
Nirmit Mishra <93557744+nirmit27@users.noreply.github.com>
2026-02-14 11:45:52 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,37 @@
+"""
+Master :::: Commit #1
+"""
+
+from enum import Enum
+from dataclasses import dataclass
+
+
+class Role(Enum):
+    ADMIN = "admin"
+    MEMBER = "member"
+
+
+@dataclass
+class User:
+    name: str
+    role: Role
+
+
+# NOTE: Driver code
+if __name__ == "__main__":
+    users = [
+        User("nirmit27", Role.ADMIN),
+        User("nirmit33", Role.MEMBER),
+        User("nirmit45", Role.ADMIN),
+        User("nirmit34", Role.ADMIN),
+        User("nirmit75", Role.MEMBER),
+        User("nirmit56", Role.MEMBER),
+    ]
+
+    admins = list(filter(lambda user: user.role == Role.ADMIN, users))
+
+    print("--- List of admins ---")
+    for i, admin in enumerate(admins):
+        print(f"    #{i + 1}. {admin.name}")
+
+    print("\n--- Driver execution complete ---")
