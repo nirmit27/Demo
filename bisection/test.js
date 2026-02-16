04ef67a67b0218306ecbef16476464ad04cb49c9
nirmit27 <mishrayyhsnm2002@gmail.com>
2026-02-16 16:33:15 +0530

    Revert "v2 working"
    This reverts commit e5c5f6bf13e1c0b2f1f8304eb9c03272deb17c85.

--- bisection/test.js
+++ bisection/test.js
@@ -1,4 +1,3 @@
 // Simulating a scenario for bisection
 
-console.log("hello friend... we live in a simulation - v4");
-throw new Error("This was a mistake");
+console.log("hello friend... we live in a simulation");
